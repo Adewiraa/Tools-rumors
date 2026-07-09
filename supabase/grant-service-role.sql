6ff46ff00191612c95678fea83f1ead8c66884fc
Ade Wiramiharja <ade.wiramiharjaa@gmail.com>
2026-07-09 14:03:51 +0700

    Remove sequence grants from Supabase setup

--- supabase/grant-service-role.sql
+++ supabase/grant-service-role.sql
@@ -6,10 +6,3 @@ grant all privileges on public.clubs to service_role;
 grant all privileges on public.club_seasons to service_role;
 grant all privileges on public.players to service_role;
 grant all privileges on public.club_rosters to service_role;
-
-grant usage, select, update on sequence public.competitions_id_seq to service_role;
-grant usage, select, update on sequence public.seasons_id_seq to service_role;
-grant usage, select, update on sequence public.clubs_id_seq to service_role;
-grant usage, select, update on sequence public.club_seasons_id_seq to service_role;
-grant usage, select, update on sequence public.players_id_seq to service_role;
-grant usage, select, update on sequence public.club_rosters_id_seq to service_role;
